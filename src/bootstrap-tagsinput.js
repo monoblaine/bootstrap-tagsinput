0efd6f86ba47a459fdcbcbd22162213307b380de
Serhan Apaydın <zerhan@gmail.com>
2016-06-08 13:51:20 +0300

    focus olayını ana fork'ta düzelttikleri için addGlowEvents olayını kaldır

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -1,15 +1,6 @@
 (function ($) {
   "use strict";
 
-  function addGlowEvents () {
-      var me = this,
-          $container = me.$container,
-          addGlow = function () { $container.addClass("focus"); },
-          removeGlow = function () { $container.removeClass("focus"); };
-
-      me.$input.focus(addGlow).blur(removeGlow);
-  }
-
   var defaultOptions = {
     tagClass: function(item) {
       return 'label label-info';
@@ -59,8 +50,6 @@
 
     this.$container = $('<div class="bootstrap-tagsinput"></div>');
     this.$input = $('<input type="text" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
-
-    addGlowEvents.call(this);
     this.stringsAsTags = this.$element.data("strings-as-tags") === true;
 
     this.$element.before(this.$container);
